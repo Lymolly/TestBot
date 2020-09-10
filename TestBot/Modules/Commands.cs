9f032346f06687ce82e12ee9fa29747e652f3afa
D3N1s1s <kozyrevd2001@ukr.net>
2020-09-10 20:45:44 +0300

    "Idi Nahui" Discord bot;)

---
+++ TestBot/Modules/Commands.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBot.Services
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("hi")]
+        public async Task Sheva()
+        {
+            var user = Context.User.Id;
+            if (user == 292613626878230528)
+            {
+                var user2 = Context.User;
+                await ReplyAsync($"[{user2.Username}], пошел нахуй, долбоеб!");
+            }
+        }
+    }
+}
--- TestBot/Program.cs
+++ TestBot/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TestBot
 {
@@ -10,6 +17,61 @@ namespace TestBot
     {
         static void Main(string[] args)
         {
+            new Program().RunBotAsync().GetAwaiter().GetResult();
+        }
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "NzUzNjQ5NjczNTgyOTM2MDg0.X1pQ9A.L6xRsc3DqqX_ZW59l-GEP8YCkp4";
+            _client.Log += _client_Log;
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(TokenType.Bot,token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage mes)
+        {
+            var message = mes as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+            int argPos = default;
+            if (message.HasStringPrefix("", ref argPos))
+            {
+                if (message.Author.Id == 292613626878230528)
+                {
+
+                }
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+           
         }
     }
 }
